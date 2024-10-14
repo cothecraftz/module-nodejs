2a32daffbceaf6948179f37ad2fff12db2b5484f
Joko Santoso <joko74479@gmail.com>
2024-10-14 22:20:30 +0700

    Learn - NodeJS day 10 - logging end

--- nodejs-logging/test/logger.test.js
+++ nodejs-logging/test/logger.test.js
@@ -1,4 +1,5 @@
 import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
 
 describe('logging', () => {
   it('create logger', () => {
@@ -37,5 +38,46 @@ describe('logging', () => {
     });
 
     logger.info('Hello World.');
+  });
+  it('file transport logger file', () => {
+    const logger = winston.createLogger({
+      level: 'debug',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      transports: [
+        new winston.transports.Console({}),
+        new winston.transports.File({
+          filename: 'transport.log',
+        }),
+        new winston.transports.File({
+          level: 'error',
+          filename: 'transport-error.log',
+        }),
+      ],
+    });
+
+    logger.info('Hello World.');
+    logger.info('Hello World.');
+    logger.error('Hello Error!.');
+    logger.error('Hello Error!.');
+  });
+
+  it('file rotate file', () => {
+    const logger = winston.createLogger({
+      level: 'info',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+      transports: [
+        new winston.transports.Console({}),
+        new DailyRotateFile({
+          filename: 'app-%Date%.log',
+          zippedArchive: true,
+          maxSize: '1m',
+          maxFiles: '14d',
+        }),
+      ],
+    });
+
+    for (let index = 0; index < 30000; index++) {
+      logger.info('Hello ' + index);
+    }
   });
 });
